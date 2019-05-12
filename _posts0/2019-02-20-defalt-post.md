9ae6e568793208ac212016b555dc33faeed35100
dodoleesj <dodoleesj@>
2019-05-12 14:49:15 +0900

    apply theme

---
+++ _posts0/2019-02-20-defalt-post.md
@@ -0,0 +1,20 @@
+---
+#type : posts
+layout: posts
+title:  "default post"
+date:   2019-02-20 22:22
+categories: 기타
+tags :
+published : false
+hidden :
+---
+<body>
+  <section>
+    포스트 내용
+  </section>
+
+  <div class="bottom-post">
+    <img src="/assets/images/category.png" class="icon" style="width:7%;"> Category :
+    <img src="/assets/images/calender.png" class="icon" style="width:5%;"> Update :
+  </div>
+</body>
---
+++ _site0/assets/js/lunr/lunr-store.js
@@ -0,0 +1,11 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when a file is updated.   To add new posts, simply add a file in the _posts directory that follows the convention YYYY-MM-DD-name-of-post.ext and includes the necessary front matter. Take a look at the source for this post to get an idea about how it works.   Jekyll also offers powerful support for code snippets:   def print_hi(name)   puts \"Hi, #{name}\" end print_hi('Tom') #=&gt; prints 'Hi, Tom' to STDOUT.  Check out the Jekyll docs for more info on how to get the most out of Jekyll. File all bugs/feature requests at Jekyll’s GitHub repo. If you have questions, you can ask them on Jekyll Talk.   ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/welcome-to-jekyll/",
+        "teaser":"http://localhost:4000/assets/images/500x300.png"},{
+        "title": "앞으로 차근차근 해 나갈 것들",
+        "excerpt":"     Dev       블로그 개선하기 - liquid, Git 공부       배운 것 복습하기 - Crawling, API 적용, Python 공부       학교 버전 LITTER LATI 개발하기          SW교육       나만의 SW 커리큘럼 만들기 - 기존 자료를 정리, 분석해서 만들기 --- SW 선도학교 운영에 적용       초등학생을 위한 짧은 유투브 컨텐츠 만들기       선생님을 위한 연수 계획하기                Category : 기타          Update :    ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/todolist/",
+        "teaser":"http://localhost:4000/assets/images/500x300.png"}]
